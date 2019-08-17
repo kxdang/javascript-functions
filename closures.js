14d8dc8e8078ff8c06278451c70c59387b62ddca
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-08-17 11:49:19 -0400

    Example of createAdder scope where createAdder function is called

--- closures.js
+++ closures.js
@@ -21,3 +21,16 @@ counter.decrement();
 counter.decrement();
 
 console.log(counter.get());
+
+//Adder
+
+const createAdder = a => {
+  return b => {
+    return a + b;
+  };
+};
+
+const add10 = createAdder(10);
+console.log(add10(-2)); // turns into 8
+console.log(add10(20)); // turns into 30
+console.log(createAdder(10)(-2));
