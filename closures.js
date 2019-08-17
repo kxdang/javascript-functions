ccf3121645ac18e19d0bbeb892f7e6c7386fa992
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-08-17 12:20:01 -0400

    Closure challenge video #110 (Andrew Mead)

--- closures.js
+++ closures.js
@@ -20,9 +20,10 @@ counter.increment();
 counter.decrement();
 counter.decrement();
 
-console.log(counter.get());
+// console.log(counter.get());
 
 //Adder
+const add = (a, b) => a + b; //standard version curried version
 
 const createAdder = a => {
   return b => {
@@ -31,6 +32,20 @@ const createAdder = a => {
 };
 
 const add10 = createAdder(10);
-console.log(add10(-2)); // turns into 8
-console.log(add10(20)); // turns into 30
-console.log(createAdder(10)(-2));
+// console.log(add10(-2)); // turns into 8
+// console.log(add10(20)); // turns into 30
+// console.log(createAdder(10)(-2)); //turns into 8
+
+// Tipper
+
+const createTipper = baseTip => {
+  return billAmount => {
+    return billAmount * baseTip;
+  };
+};
+
+const tip15 = createTipper(0.15);
+const tip20 = createTipper(0.2);
+
+console.log(tip15(100));
+console.log(tip20(200));
